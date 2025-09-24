b59e3dd114cdd4e9c960fc35b80ce98262fd41f0
Nika Chitava <student@eu.local>
2025-09-24 20:02:57 +0400

    finished

---
+++ main.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+type = str(input("Enter a type (signed/unsigned): "))
+
+if type == "signed":
+    binary = bin(number)[2:]
+elif type == "unsigned":
+    if number < 0:
+        binary = bin((1 << 32) + number)[2:]
+    else:
+        binary = bin(number)[2:]
+        
+print(f"Number: {binary}, Type: {type}")
